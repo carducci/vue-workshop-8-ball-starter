98c782ad1cc4a9fcbd04743f73eeb0ed8307289f
view class <vue@example.com>
2020-06-02 20:46:28 +0000

    moved theAnswerSlot CSS
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>
    Co-Authored-By: Kinna <kinnat@users.noreply.github.com>
    Co-Authored-By: Diana McIntosh <dotteddice@users.noreply.github.com>

--- src/components/TheAnswerSlot.vue
+++ src/components/TheAnswerSlot.vue
@@ -15,5 +15,25 @@
 </script>
 
 <style scoped>
-
+    .answer-slot {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        position: absolute;
+        margin: -200px 0 0 -200px;
+        border-radius: 50%;
+        width: 60%;
+        height: 60%;
+        background: #1a529a;
+        left: 70%;
+        font-family: Helvetica;
+        top: 70%;
+        text-align: center;
+        color: rgba(255, 255, 255, 0.57);
+        line-height: 1.25;
+        font-size: 23px;
+        -moz-box-shadow: inset 0 0 10px #000000;
+        -webkit-box-shadow: inset 0 0 10px #000000;
+        box-shadow: inset 0 0 50px #000000;
+    }
 </style>
