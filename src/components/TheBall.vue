dd922c3ee692e95ca956a9dcb50b03715f3bb6d2
view class <vue@example.com>
2020-06-02 18:38:28 +0000

    Shakable ball!
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>
    Co-Authored-By: Kinna <kinnat@users.noreply.github.com>
    Co-Authored-By: Diana McIntosh <dotteddice@users.noreply.github.com>

--- src/components/TheBall.vue
+++ src/components/TheBall.vue
@@ -1,7 +1,11 @@
 <template>
-    <div class="ball black">
-    <div class="answer-slot">{{ getAnswer() }}</div>
+    <div>
+        <div class="ball black">
+            <div class="answer-slot">{{ answer }}</div>
+        </div>
+        <button v-on:click="shake">Shake It!!!</button>
     </div>
+
 </template>
 
 <script>
@@ -9,19 +13,23 @@
         name: "TheBall",
         data() {
             return {
+                answer: "Shake to reveal your destiny..."
             }
         },
         methods: {
             getAnswer() {
                 Array.prototype.random = function () {
-                    return this[Math.floor(Math.random()*this.length)];
+                    return this[Math.floor(Math.random() * this.length)];
                 }
-                const answers = ["one", "two", "three"];
+                const answers = ["Yes", "No", "Ask Again", "Not today!"];
                 return answers.random();
+            },
+            shake() {
+                this.answer = this.getAnswer();
             }
         }
     }
-    //Put in our answers
+
     //A way to shake
 
 </script>
