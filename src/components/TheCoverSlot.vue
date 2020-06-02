e9852b9ef429ef88b01a4f395443292df7b5042c
view class <vue@example.com>
2020-06-02 20:45:00 +0000

    Moved CoverSlot CSS
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>
    Co-Authored-By: Kinna <kinnat@users.noreply.github.com>
    Co-Authored-By: Diana McIntosh <dotteddice@users.noreply.github.com>

--- src/components/TheCoverSlot.vue
+++ src/components/TheCoverSlot.vue
@@ -9,5 +9,19 @@
 </script>
 
 <style scoped>
-
+    .cover-slot {
+        position: absolute;
+        margin: -200px 0 0 -200px;
+        border-radius: 50%;
+        width: 50%;
+        height: 50%;
+        background: white;
+        left: 75%;
+        font-family: Helvetica;
+        top: 75%;
+        text-align: center;
+        color: black;
+        line-height: 1.35;
+        font-size: 150px;
+    }
 </style>
